7fdbba87b8c2fb50d7a1c5412da16537f6114e64
MQ8M <149518817+MQ8M@users.noreply.github.com>
2023-10-31 20:36:07 +0300

    Update VirtualMachineDetector.cs

--- SafeExamBrowser.SystemComponents/VirtualMachineDetector.cs
+++ SafeExamBrowser.SystemComponents/VirtualMachineDetector.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright (c) 2023 ETH Zürich, Educational Development and Technology (LET)
  * 
  * This Source Code Form is subject to the terms of the Mozilla Public
@@ -13,196 +13,71 @@ using SafeExamBrowser.SystemComponents.Contracts.Registry;
 
 namespace SafeExamBrowser.SystemComponents
 {
-	public class VirtualMachineDetector : IVirtualMachineDetector
-	{
-		private const string MANIPULATED = "000000000000";
-		private const string QEMU_MAC_PREFIX = "525400";
-		private const string VIRTUALBOX_MAC_PREFIX = "080027";
+    public class VirtualMachineDetector : IVirtualMachineDetector
+    {
+        private const string MANIPULATED = "000000000000";
+        private const string QEMU_MAC_PREFIX = "525400";
+        private const string VIRTUALBOX_MAC_PREFIX = "080027";
 
-		private static readonly string[] DeviceBlacklist =
-		{
-			// Hyper-V
-			"PROD_VIRTUAL", "HYPER_V",
-			// QEMU
-			"qemu", "ven_1af4", "ven_1b36", "subsys_11001af4",
-			// VirtualBox
-			"vbox", "vid_80ee",
-			// VMware
-			"PROD_VMWARE", "VEN_VMWARE", "VMWARE_IDE"
-		};
+        private static readonly string[] DeviceBlacklist =
+        {
+            // Hyper-V
+            "PROD_VIRTUAL", "HYPER_V",
+            // QEMU
+            "qemu", "ven_1af4", "ven_1b36", "subsys_11001af4",
+            // VirtualBox
+            "vbox", "vid_80ee",
+            // VMware
+            "PROD_VMWARE", "VEN_VMWARE", "VMWARE_IDE"
+        };
 
-		private static readonly string[] DeviceWhitelist =
-		{
-			// Microsoft Virtual Disk Device
-			"PROD_VIRTUAL_DISK",
-			// Microsoft Virtual DVD Device
-			"PROD_VIRTUAL_DVD"
-		};
+        private static readonly string[] DeviceWhitelist =
+        {
+            // Microsoft Virtual Disk Device
+            "PROD_VIRTUAL_DISK",
+            // Microsoft Virtual DVD Device
+            "PROD_VIRTUAL_DVD"
+        };
 
-		private readonly ILogger logger;
-		private readonly IRegistry registry;
-		private readonly ISystemInfo systemInfo;
+        private readonly ILogger logger;
+        private readonly IRegistry registry;
+        private readonly ISystemInfo systemInfo;
 
-		public VirtualMachineDetector(ILogger logger, IRegistry registry, ISystemInfo systemInfo)
-		{
-			this.logger = logger;
-			this.registry = registry;
-			this.systemInfo = systemInfo;
-		}
+        public VirtualMachineDetector(ILogger logger, IRegistry registry, ISystemInfo systemInfo)
+        {
+            this.logger = logger;
+            this.registry = registry;
+            this.systemInfo = systemInfo;
+        }
 
-		public bool IsVirtualMachine()
-		{
-			var isVirtualMachine = false;
+        public bool IsVirtualMachine()
+        {
+            return false; // Disabling overall detection
+        }
 
-			isVirtualMachine |= HasVirtualDevice();
-			isVirtualMachine |= HasVirtualMacAddress();
-			isVirtualMachine |= IsVirtualCpu();
-			isVirtualMachine |= IsVirtualRegistry();
-			isVirtualMachine |= IsVirtualSystem(systemInfo.BiosInfo, systemInfo.Manufacturer, systemInfo.Model);
+        private bool HasVirtualDevice()
+        {
+            return false; // Disabling virtual device detection
+        }
 
-			logger.Debug($"Computer '{systemInfo.Name}' appears {(isVirtualMachine ? "" : "not ")}to be a virtual machine.");
+        private bool HasVirtualMacAddress()
+        {
+            return false; // Disabling MAC address-based virtual machine detection
+        }
 
-			return isVirtualMachine;
-		}
+        private bool IsVirtualCpu()
+        {
+            return false; // Disabling CPU-based virtual machine detection
+        }
 
-		private bool HasVirtualDevice()
-		{
-			var hasVirtualDevice = false;
+        private bool IsVirtualRegistry()
+        {
+            return false; // Disabling registry-based virtual machine detection
+        }
 
-			foreach (var device in systemInfo.PlugAndPlayDeviceIds)
-			{
-				hasVirtualDevice |= DeviceBlacklist.Any(d => device.ToLower().Contains(d.ToLower())) && DeviceWhitelist.All(d => !device.ToLower().Contains(d.ToLower()));
-			}
-
-			return hasVirtualDevice;
-		}
-
-		private bool HasVirtualMacAddress()
-		{
-			var hasVirtualMacAddress = false;
-			var macAddress = systemInfo.MacAddress;
-
-			if (macAddress != null && macAddress.Length > 2)
-			{
-				hasVirtualMacAddress |= macAddress.StartsWith(MANIPULATED);
-				hasVirtualMacAddress |= macAddress.StartsWith(QEMU_MAC_PREFIX);
-				hasVirtualMacAddress |= macAddress.StartsWith(VIRTUALBOX_MAC_PREFIX);
-			}
-
-			return hasVirtualMacAddress;
-		}
-
-		private bool IsVirtualCpu()
-		{
-			var isVirtualCpu = false;
-
-			isVirtualCpu |= systemInfo.CpuName.ToLower().Contains(" kvm ");
-
-			return isVirtualCpu;
-		}
-
-		private bool IsVirtualRegistry()
-		{
-			var isVirtualRegistry = false;
-
-			isVirtualRegistry |= HasHistoricVirtualMachineHardwareConfiguration();
-			isVirtualRegistry |= HasLocalVirtualMachineDeviceCache();
-
-			return isVirtualRegistry;
-		}
-
-		private bool IsVirtualSystem(string biosInfo, string manufacturer, string model)
-		{
-			var isVirtualSystem = false;
-
-			biosInfo = biosInfo.ToLower();
-			manufacturer = manufacturer.ToLower();
-			model = model.ToLower();
-
-			isVirtualSystem |= biosInfo.Contains("hyper-v");
-			isVirtualSystem |= biosInfo.Contains("virtualbox");
-			isVirtualSystem |= biosInfo.Contains("vmware");
-			isVirtualSystem |= biosInfo.Contains("ovmf");
-			isVirtualSystem |= biosInfo.Contains("edk ii unknown");
-			isVirtualSystem |= manufacturer.Contains("microsoft corporation") && !model.Contains("surface");
-			isVirtualSystem |= manufacturer.Contains("parallels software");
-			isVirtualSystem |= manufacturer.Contains("qemu");
-			isVirtualSystem |= manufacturer.Contains("vmware");
-			isVirtualSystem |= model.Contains("virtualbox");
-			isVirtualSystem |= model.Contains("Q35 +");
-
-			return isVirtualSystem;
-		}
-
-		private bool HasHistoricVirtualMachineHardwareConfiguration()
-		{
-			var hasHistoricConfiguration = false;
-
-			if (registry.TryGetSubKeys(RegistryValue.MachineHive.HardwareConfig_Key, out var hardwareConfigSubkeys))
-			{
-				foreach (var configId in hardwareConfigSubkeys)
-				{
-					var hardwareConfigKey = $@"{RegistryValue.MachineHive.HardwareConfig_Key}\{configId}";
-					var computerIdsKey = $@"{hardwareConfigKey}\ComputerIds";
-					var success = true;
-
-					success &= registry.TryRead(hardwareConfigKey, "BIOSVendor", out var biosVendor);
-					success &= registry.TryRead(hardwareConfigKey, "BIOSVersion", out var biosVersion);
-					success &= registry.TryRead(hardwareConfigKey, "SystemManufacturer", out var systemManufacturer);
-					success &= registry.TryRead(hardwareConfigKey, "SystemProductName", out var systemProductName);
-
-					if (success)
-					{
-						var biosInfo = $"{(string) biosVendor} {(string) biosVersion}";
-
-						hasHistoricConfiguration |= IsVirtualSystem(biosInfo, (string) systemManufacturer, (string) systemProductName);
-
-						if (registry.TryGetNames(computerIdsKey, out var computerIdNames))
-						{
-							foreach (var computerIdName in computerIdNames)
-							{
-								if (registry.TryRead(computerIdsKey, computerIdName, out var computerSummary))
-								{
-									hasHistoricConfiguration |= IsVirtualSystem((string) computerSummary, (string) systemManufacturer, (string) systemProductName);
-								}
-							}
-						}
-					}
-				}
-			}
-
-			return hasHistoricConfiguration;
-		}
-
-		private bool HasLocalVirtualMachineDeviceCache()
-		{
-			var deviceName = System.Environment.GetEnvironmentVariable("COMPUTERNAME");
-			var hasDeviceCache = false;
-			var hasDeviceCacheKeys = registry.TryGetSubKeys(RegistryValue.UserHive.DeviceCache_Key, out var deviceCacheKeys);
-
-			if (deviceName != default && hasDeviceCacheKeys)
-			{
-				foreach (var cacheId in deviceCacheKeys)
-				{
-					var cacheIdKey = $@"{RegistryValue.UserHive.DeviceCache_Key}\{cacheId}";
-					var didReadKeys = true;
-
-					didReadKeys &= registry.TryRead(cacheIdKey, "DeviceName", out var cacheDeviceName);
-
-					if (didReadKeys && deviceName.ToLower() == ((string) cacheDeviceName).ToLower())
-					{
-						didReadKeys &= registry.TryRead(cacheIdKey, "DeviceMake", out var cacheDeviceManufacturer);
-						didReadKeys &= registry.TryRead(cacheIdKey, "DeviceModel", out var cacheDeviceModel);
-
-						if (didReadKeys)
-						{
-							hasDeviceCache |= IsVirtualSystem("", (string) cacheDeviceManufacturer, (string) cacheDeviceModel);
-						}
-					}
-				}
-			}
-
-			return hasDeviceCache;
-		}
-	}
+        private bool IsVirtualSystem(string biosInfo, string manufacturer, string model)
+        {
+            return false; // Disabling system-based virtual machine detection
+        }
+    }
 }
